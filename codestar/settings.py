9b44d9ed9de00dff054fd40548786cc086660c07
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-07-30 22:42:18 +0200

    fix project url.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", 'dev-only-key-please-set-env-var')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1'
