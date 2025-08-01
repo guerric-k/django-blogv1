1f74825f7575449d5abf5b54cbffe40fe16071e1
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-08-02 01:26:38 +0200

    add static files

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 # URL patterns for the blog app
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
